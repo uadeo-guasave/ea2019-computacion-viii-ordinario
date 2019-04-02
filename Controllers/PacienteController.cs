8b2b0d56b878e2045bbccc79bca1cbce41d7792a
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-04-02 11:50:11 -0700

    CRUD
    Finalización del ejemplo de Altas Bajas y Cambios

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -36,21 +36,55 @@ namespace C8ProyectoFinalPorEquipos.Controllers
             return RedirectToAction("Index");
         }
 
+        // https://localhost:5001/Paciente/Edit/1
         [HttpGet]
         public IActionResult Edit(int id)
         {
-
+            using (var db = new SqliteDbContext())
+            {
+                var paciente = db.Pacientes.Find(id);
+                if (paciente != null)
+                {
+                    return View(paciente);
+                }
+            }
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         public IActionResult Edit(int id, PacienteViewModel paciente)
         {
-            
+            if (ModelState.IsValid && id > 0)
+            {
+                using (var db = new SqliteDbContext())
+                {
+                    var pacienteExistente = db.Pacientes.Find(id);
+                    if (pacienteExistente != null)
+                    {
+                        pacienteExistente.Nombre = paciente.Nombre;
+                        pacienteExistente.Apellidos = paciente.Apellidos;
+                        pacienteExistente.Edad = paciente.Edad;
+                        pacienteExistente.Genero = paciente.Genero;
+                        db.SaveChanges();
+                    }
+                }
+            }
+            return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
         {
+            if (id > 0)
+            {
+                using (var db = new SqliteDbContext())
+                {
+                    var paciente = db.Pacientes.Find(id);
+                    db.Remove(paciente);
+                    db.SaveChanges();
+                }
+            }
 
+            return RedirectToAction("Index");
         }
     }
 }
